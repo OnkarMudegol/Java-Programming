5afe6af71c311f9b8d147636f12069d4d8abda27
OnkarMudegol <omudegol@gmail.com>
2024-02-07 12:13:38 +0530

    functionality of intializer block

--- intializerBlock.java
+++ intializerBlock.java
@@ -1,20 +1,20 @@
-public class Intializer_block{
+public class intializerBlock{
 //Intializer block, position does not matter
 {
     System.out.println("Common part of every concstructor invoked!");
 }
-public Intializer_block()
+public intializerBlock()
 {
     System.out.println("Default Constructor invoked!!");
 }
-public Intializer_block(int x)
+public intializerBlock(int x)
 {
     System.out.println("Parameterized Constructor invoked!!");
 }
 public static void main(String args[])
 {
-    Intializer_block obj1, obj2;
-    obj1 = new Intializer_block();
-    obj2 = new Intializer_block(0);
+    intializerBlock obj1, obj2;
+    obj1 = new intializerBlock();
+    obj2 = new intializerBlock(0);
 }
 }
