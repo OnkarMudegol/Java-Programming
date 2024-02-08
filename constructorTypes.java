91f2646d86123efc1708a5824df56654f386f699
OnkarMudegol <omudegol@gmail.com>
2024-02-08 13:18:49 +0530

    types of constructors

---
+++ constructorTypes.java
@@ -0,0 +1,42 @@
+// public class constructorTypes
+// {
+//     constructorTypes(String name)public class constructorTypes
+//     {
+//     constructorTypes(String name)
+//     {
+//         System.out.println("Constructor with one"+"argument -String: "+name);
+//     }
+//     constructorTypes(String name,int age)
+//     {
+//         System.out.println("Constructor with two"+"arguments -String and Integer: "+name+" "+age);
+//     }
+//     constructorTypes(Long id)
+//     {
+//         System.out.println("Constructor with one"+"argument -Long: "+id);
+//     }
+//     public static void main(String args[])
+//     {
+//         constructorTypes obj2=new constructorTypes("Vijay");
+//         constructorTypes obj3=new constructorTypes("Vijay",'A');
+//         constructorTypes obj4=new constructorTypes(9999999999999999);
+//     }
+// }
+//     {
+//         System.out.println("Constructor with one"+"argument -String: "+name);
+//     }
+//     constructorTypes(String name,int age)
+//     {
+//         System.out.println("Constructor with two"+"arguments -String and Integer: "+name+" "+age);
+//     }
+//     constructorTypes(Long id)
+//     {
+//         System.out.println("Constructor with one"+"argument -Long: "+id);
+//     }
+//     public static void main(String args[])
+//     {
+//         constructorTypes obj2=new constructorTypes("Vijay");
+//         constructorTypes obj3=new constructorTypes("Vijay",'A');
+//         constructorTypes obj4=new constructorTypes(9999999999999999);
+//     }
+// }
+
