67548513bb77aa5a8b1c18be56ba61f11266551b
OnkarMudegol <omudegol@gmail.com>
2024-02-22 14:30:14 +0530

    abstract class

---
+++ abstract class/abstractClass.java
@@ -0,0 +1,57 @@
+abstract class A
+{
+    A()
+    {
+        System.out.println("COnstructor of A");
+    }
+    abstract void display();
+    void C()
+    {
+        System.out.println("Non abstract member function of abstrct class A");
+    }
+    final void Final()  //This is the final definition, it cannot be overwritten in member
+                    //classes
+    {
+        System.out.println("Final funtion of abstract class A");
+    }
+    static void stat()  //Can be called directy without creating a object of A
+    {
+        System.out.println("Static memenr funtion of the abstrct class A");
+    }
+}
+class B extends A 
+{
+    B()
+    {
+        System.out.println("Constructor of B");
+    }
+    void display()
+    {
+        System.out.println("Viewing class B");
+    }
+}
+class C extends A 
+{
+    C()
+    {
+        System.out.println("Constructor of C");
+    }
+    void display()
+    {
+        System.out.println("Viewing class C");
+    }
+}
+public class abstractClass
+{
+    public static void main(String args[])
+    {
+        A a = new B();
+        a.display();
+        a.C();
+        A b = new C();
+        b.display();
+        b.Final();
+        A.stat(); //Called without craeting a object, dierct approach
+
+    }
+}
