51cb6c613c50bc4e245e346f1edd5b25ab48f1e5
OnkarMudegol <omudegol@gmail.com>
2024-02-26 12:36:41 +0530

    runtime polymorphism

---
+++ polymorphism/runtime.java
@@ -0,0 +1,44 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class Pig extends Animal {
+    public void makeSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("The dog says: woof woof");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();  // create an Animal object
+        Animal myPig = new Pig();  // create a Pig object
+        Animal myDog = new Dog();  // create a Dog object
+
+        myAnimal.makeSound();  // call the makeSound method of the Animal class
+        myPig.makeSound();  // call the makeSound method of the Pig class
+        myDog.makeSound();  // call the makeSound method of the Dog class
+    }
+}
+
+class 1
+{
+    class Helper
+    {
+        static int multiply(int a, int b)
+        {
+            return a*b;
+        }
+        static double multiply(double a, double b)
+        {
+            return a*b;
+        }
+    }
+}
