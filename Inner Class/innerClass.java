aea9853d9d6d8a5915438a4df8ce06b765f83035
OnkarMudegol <omudegol@gmail.com>
2024-03-13 12:00:06 +0530

    Introduction to inner classes

--- Inner Class/innerClass.java
+++ Inner Class/innerClass.java
@@ -1,11 +1,35 @@
 //This concept is majorly used for encapsulation. Helps group all the related classes in one place.
-class Outer
-{
-    class Inner
+// class Outer
+// {
+//     class Inner
+//     {
+//         public void show()
+//         {
+//             System.out.println("In a nested class method.");
+//         }
+//     }
+// }
+// class innerClass
+// {
+//     public static void main(String args[])
+//     {
+//         Outer.Inner obj = new Outer().new Inner();
+//         obj.show();
+//     }    
+// }
+
+
+//Another way to create variables 
+class Outer{
+    private int data=30;
+    void msg1()
     {
-        public void show()
+        System.out.println("In Outer class.");
+    }
+    class Inner{
+        void msg()
         {
-            System.out.println("In a nested class method.");
+            System.out.println("The value of data is: "+data);
         }
     }
 }
@@ -13,7 +37,9 @@ class innerClass
 {
     public static void main(String args[])
     {
-        Outer.Inner obj = new Outer().new Inner();
-        obj.show();
-    }    
+        Outer obj = new Outer();
+        Outer.Inner obj2 = new Outer().new Inner();
+        obj.msg1();
+        obj2.msg();
+    }
 }
