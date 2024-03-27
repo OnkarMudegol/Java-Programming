91b296c91d7115be8d063db6ab13318843d90ba9
OnkarMudegol <omudegol@gmail.com>
2024-03-27 12:07:17 +0530

    Intoduction to Interface.

--- Inheritance/Interface/interfaceIntro.java
+++ Inheritance/Interface/interfaceIntro.java
@@ -1,5 +1,7 @@
-// Not necessary but helps you find the error in case your interface does not follow the rules
-//SAM - Single Abstract Method; Functional interface is a interface containing a single abstract method, can conatin any number of other methods, doesnt matter
+//Note:
+//1.the tag is not necessary but helps you find the error in case your interface does not follow the rules, helps check the rule of only one abstract method
+//2.SAM - Single Abstract Method; Functional interface is a interface containing a single abstract method, can conatin any number of other methods, doesnt matter
+//3.A functional interface can extend another interface only if it does not have any abstract method in it
 @FunctionalInterface
 interface API
 {
