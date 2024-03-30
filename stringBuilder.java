a79935f8707a8e4f25af08bf1c78840a75efa311
OnkarMudegol <omudegol@gmail.com>
2024-03-30 10:17:06 +0530

    Reversing a string using StringBuilder

---
+++ stringBuilder.java
@@ -0,0 +1,10 @@
+public class Practice
+{
+    public static void main(String args[])
+    {
+        String ab = "Onkar";
+        StringBuilder sb = new StringBuilder(ab);
+        sb.reverse();
+        System.out.println(sb.toString());
+    }
+}
