dddbcc1915a365f4a7cd636d2b40f723ec0ee7e1
OnkarMudegol <omudegol@gmail.com>
2024-04-03 11:34:34 +0530

    Types of exceptions.

---
+++ Exceptions/fileError.java
@@ -0,0 +1,9 @@
+//This is a checked exception, the file does not exist
+import java.io.FileInputStream;
+class fileError
+{
+	public static void main(String args[])
+	{
+		FileInputStream abc = new FileInputStream("C:/Users/omude/OneDrive/Desktop/Java -Programming/xtz.txt");
+	}
+}
