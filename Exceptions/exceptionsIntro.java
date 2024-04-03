b5c0d8578b627b922b36e240b6f444f97fd68be6
OnkarMudegol <omudegol@gmail.com>
2024-04-03 12:09:33 +0530

    Intoduction to Exceptions

--- Exceptions/exceptionsIntro.java
+++ Exceptions/exceptionsIntro.java
@@ -1,6 +1,7 @@
 //Exceptions are of two types, checked and unchecked
 //Arithematic exceptions are unchceked exceptions, which means the program will be compiled, exception will be thrown at run time
-//Checked excceptions are the exceptions about which the compiler will alert us wile compiling. Unchecked are the ones which the compiler does'nt know about.
+//Checked excceptions are the exceptions about which the compiler will alert us wile compiling. Unchecked are the ones which the compiler does'nt know about. 
+//Note: Both exceptions will occur at run time, it is just that the compiler warns us about the  potential problem in checked exceptions
 public class exceptionsIntro
 {
     public static void main(String args[])
