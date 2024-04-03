c19fba151acdab65d593cb40d7c467ca27a3763a
OnkarMudegol <omudegol@gmail.com>
2024-04-03 11:36:37 +0530

    handling exceptions

---
+++ Exceptions/exceptionHandling.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+public class exceptionHandling
+{
+    public static void main(String args[])
+	{
+        try{
+            FileInputStream abc = new FileInputStream("C:/Users/omude/OneDrive/Desktop/Java -Programming/xtz.txt");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Check if the file exists or not!!");
+        }
+		
+	}
+}
