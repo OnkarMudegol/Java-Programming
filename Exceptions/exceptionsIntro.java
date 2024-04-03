20fb673427be156333dd521430c727eb34c8f6b0
OnkarMudegol <omudegol@gmail.com>
2024-04-03 11:27:44 +0530

    Intorduction to errors

---
+++ Exceptions/exceptionsIntro.java
@@ -0,0 +1,17 @@
+//Exceptions are of two types, checked and unchecked
+//Arithematic exceptions are unchceked exceptions, which means the program will be compiled, exception will be thrown at
+//run time
+public class exceptionsIntro
+{
+    public static void main(String args[])
+    {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println(100/0);
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+    }
+}
