ae53efa30326ac21af53494aa095216cd13c94b4
OnkarMudegol <omudegol@gmail.com>
2024-04-03 12:49:32 +0530

    Implementation of finally block

--- Exceptions/finallyBlock.java
+++ Exceptions/finallyBlock.java
@@ -11,10 +11,16 @@ public class finallyBlock
             System.out.println(100/0);
         }
         //Catch block is optional, if exception : finally folowed by exception msg
-        // catch(Exception e)
-        // {
-        //     System.out.println("Inside catch block");
-        // }
+        //We can have multiple catch block for different type of exceptions, but one exception can be caught only once. 
+        //Here if we put the Exception class one first then the Arithematic class after, it will give error as our exception is already caught once by the Exception superclass, leaving nothing for the ArithematicException subclass
+        catch(ArithematicException e)
+        {
+            System.out.println("Inside catch block 1");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Inside catch block 2");
+        }
         finally{
             System.out.println("Inside finally block");
         }
