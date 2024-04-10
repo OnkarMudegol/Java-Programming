8bff72af6ba1ff20a1d8a58c84da35ab3e390984
OnkarMudegol <omudegol@gmail.com>
2024-04-10 12:16:26 +0530

    bufferedreader implementation.

--- I/O Stream/bufferedReader.java
+++ I/O Stream/bufferedReader.java
@@ -1,13 +1,39 @@
 import java.io.*;
+// class bufferedReader{
+//     public static void main(String[] args) throws IOException
+//     {
+//         char c;
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         System.out.println("Enter character, 'q' to quit.");
+//         do{
+//             c = (char)br.read();
+//             System.out.println(c);
+//         }while(c!='q');
+//     }
+// }
+
 class bufferedReader{
-    public static void main(String[] args) throws IOException
+    String name;
+    int rollNo;
+    double fee;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    void readData() throws IOException{
+        System.out.print("Name : ");
+        name=br.readLine();
+        System.out.print("Roll No : ");
+        rollNo=Integer.parseInt(br.readLine());
+        System.out.print("Fees paid: ");
+        fee=Double.parseDouble(br.readLine());
+    }
+    void displayData(){
+        System.out.println("\nName : "+name);
+        System.out.println("Roll No : "+rollNo);
+        System.out.println("Fee Paid : "+fee+"\n");
+    }
+    public static void main(String arg[]) throws IOException
     {
-        char c;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter character, 'q' to quit.");
-        do{
-            c = (char)br.read();
-            System.out.println(c);
-        }while(c!='q');
+        bufferedReader obj = new bufferedReader();
+        obj.readData();
+        obj.displayData();
     }
 }
