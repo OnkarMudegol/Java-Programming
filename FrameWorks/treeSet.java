d5516069837e26b374f9c1f9785b035d5da21211
OnkarMudegol <omudegol@gmail.com>
2024-04-29 12:55:51 +0530

    TreeSet implementation.

--- FrameWorks/treeSet.java
+++ FrameWorks/treeSet.java
@@ -6,9 +6,11 @@ class treeSet{
         set.add(40);
         set.add(10);
         set.add(30);
+        //Usage of iterator is not necessary
         Iterator itr = set.iterator();
         while(itr.hasNext()){
             System.out.println(itr.next());
         }
+        System.out.println(set);
     }
 }
