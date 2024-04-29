b143153e59c0f5f07ee2a55bae5a133eda138019
OnkarMudegol <omudegol@gmail.com>
2024-04-29 12:44:49 +0530

    List implementation.

---
+++ FrameWorks/list.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class list{
+    public static void main(String args[]){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Ravi");
+        list.add("Vijay");
+        list.add("Ravi");
+        list.add("Ajay");
+        Iterator itr = list.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
